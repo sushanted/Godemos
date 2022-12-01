4021fa28fdc2df179ef4b41b9adac46f1e8036a7
sravale <sravale@carbonblack.com>
2022-12-01 16:29:41 +0530

    diff

--- L_06_Interfaces/Exper.go
+++ L_06_Interfaces/Exper.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	colorMap := map[string]int{
+		"red":    1,
+		"green":  2,
+		"blue":   3,
+		"yellow": 4,
+	}
+
+	// Simple iteration, we get all values
+	for key := range colorMap {
+		fmt.Printf("Color is %s\n", key)
+	}
 }
--- L_06_Interfaces/InterfaceNilReference.go
+++ L_06_Interfaces/InterfaceNilReference.go
@@ -31,6 +31,13 @@ func main() {
 
 	fmt.Println("Trying with interface")
 
+	fmt.Printf("Type: %T\n", personInfo) // This is not nil
+
+	fmt.Printf("Value: %v\n", personInfo) // This would be nil
+
+	fmt.Printf("Is nil: %v\n", personInfo == nil) // This is not overall nil as it contains the type as Person
+
+	// This would cause a fault
 	if personInfo != nil {
 		fmt.Println(personInfo.String())
 	}
